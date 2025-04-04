51c112b890f004ec83d37a424cf16674f1411a78
Jyotirmoy Saha Aditya <adityajyotirmoysaha@gmail.com>
2025-04-04 23:54:08 +0600

    ACP

---
+++ ACP.js
@@ -0,0 +1,33 @@
+function clickFunction() {
+  let bg1 = document.getElementById("background");
+
+  let colors = ["red", "green", "blue", "yellow", "indigo", "brown", "rainbow"];
+  const colorIndex = parseInt(Math.random() * colors.length);
+
+  bg1.style.backgroundColor = colors[colorIndex];
+
+  checkIfColorsMatch();
+}
+
+function clickFunction2() {
+  let bg2 = document.getElementById("backg2");
+
+  let colors = ["red", "green", "blue", "yellow", "indigo", "brown", "rainbow"];
+  const colorIndex = parseInt(Math.random() * colors.length);
+
+  bg2.style.backgroundColor = colors[colorIndex];
+
+  checkIfColorsMatch();
+}
+
+function checkIfColorsMatch() {
+  let bg1 = document.getElementById("background");
+  let bg2 = document.getElementById("backg2");
+  let stats = document.getElementById("stats");
+
+  if (bg1.style.backgroundColor === bg2.style.backgroundColor) {
+    stats.innerHTML = "Both Colors are Equal";
+  } else {
+    stats.innerHTML = "Not Equal";
+  }
+}
